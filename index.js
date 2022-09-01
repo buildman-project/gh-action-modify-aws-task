15a87b764f156359a9d909ecb358c0be86f6625c
ADRIAN DIAZ <adrian.diaz@globant.com>
2022-08-31 23:20:13 -0500

    chore: action update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const core = require("@actions/core");
+const gh = require("@actions/github");
+
+const baseString = core.getInput("base-string");
+const searchedString = core.getInput("searched-word");
+const replaceByString = core.getInput("replace-by");
+
+const replaceVariables = (base, search, replace) => {
+  return base.split(search).join(replace);
+};
+
 const addEnvVariables = () => {
   var taskDefinition = JSON.parse(fs.readFileSync(filename).toString());
   console.log(taskDefinition);
   fs.writeFileSync(filename, JSON.stringify(taskDefinition));
 };
 
-console.log("hehe");
+core.setOutput(
+  "replaced-string",
+  replaceVariables(baseString, searchedString, replaceByString)
+);
